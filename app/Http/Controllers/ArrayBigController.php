e1d2980b5f3909acc3843b1915863d2560b4d241
RobertoVida <roberto50005000@gmail.com>
2022-10-15 21:58:29 -0300

    sáb, 15 de out de 2022 21:58:29

---
+++ app/Http/Controllers/ArrayBigController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArrayBigController extends Controller
+{
+    public static $arrayTumb = [
+        'https://images7.alphacoders.com/370/thumbbig-370408.webp',
+        'https://images3.alphacoders.com/473/thumbbig-47399.webp',
+        'https://images4.alphacoders.com/889/thumbbig-889823.webp',
+        'https://images5.alphacoders.com/322/thumbbig-322198.webp',
+        'https://images3.alphacoders.com/699/thumbbig-699057.webp',
+        'https://images7.alphacoders.com/389/thumbbig-389072.webp',
+        'https://images7.alphacoders.com/536/thumbbig-536426.webp',
+        'https://images2.alphacoders.com/602/thumbbig-602223.webp',
+        'https://images2.alphacoders.com/611/thumbbig-611981.webp',
+        'https://images.alphacoders.com/472/thumbbig-47271.webp',
+        'https://images6.alphacoders.com/717/thumbbig-717752.webp',
+        'https://images5.alphacoders.com/540/thumbbig-540654.webp',
+        'https://images.alphacoders.com/673/thumbbig-673328.webp',
+        'https://images7.alphacoders.com/941/thumbbig-941884.webp',
+        'https://images4.alphacoders.com/109/thumbbig-109174.webp',
+        'https://images4.alphacoders.com/587/thumbbig-587526.webp',
+        'https://images5.alphacoders.com/690/thumbbig-690653.webp',
+        'https://images4.alphacoders.com/727/thumbbig-727745.webp',
+        'https://images8.alphacoders.com/570/thumbbig-570408.webp',
+        'https://images4.alphacoders.com/600/thumbbig-600528.webp',
+        'https://images5.alphacoders.com/564/thumbbig-564931.webp',
+        'https://images7.alphacoders.com/320/thumbbig-320623.webp',
+        'https://images6.alphacoders.com/570/thumbbig-570163.webp',
+        'https://images4.alphacoders.com/120/thumbbig-120558.webp',
+        'https://images7.alphacoders.com/587/thumbbig-587593.webp',
+        'https://images2.alphacoders.com/710/thumbbig-710137.webp',
+        'https://images4.alphacoders.com/731/thumbbig-731377.webp',
+        'https://images6.alphacoders.com/918/thumbbig-918287.webp',
+        'https://images.alphacoders.com/743/thumbbig-74381.webp',
+        'https://images6.alphacoders.com/704/thumbbig-704120.webp',
+        'https://images8.alphacoders.com/702/thumbbig-702883.webp',
+        'https://images.alphacoders.com/681/thumbbig-681587.webp'
+    ];
+    public static $arrayGender = [
+        'Mundo aberto',
+        'Aventura',
+        'Esportes',
+        'Corrida',
+        'FPS',
+        'RPG'
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Http\Controllers\ArrayBigController;
+use App\Models\Reviews;
+use App\Models\Videogames;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +19,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
+        $arrayThumb = ArrayBigController::$arrayTumb;
+        $gender = ArrayBigController::$arrayGender;
+        $developer = ['EA', 'Activision', 'Ubisoft', 'Sony'];
+
+        for($i = 0; $i < 200; $i++)
+        {
+            $reviews = Reviews::create([
+                'name_review' => $faker->name(),
+                'desc_review' => $faker->text(200),
+                'thumb' => $arrayThumb[rand(0, 31)],
+                'date_review' => date('y-m-d'),
+                'rate' => rand(1, 5),
+                'fk_id_users' => rand(1, 2)
+            ]);
+            $videogame = Videogames::create([
+                'name_game' => $faker->text(18),
+                'developer' => $developer[rand(0, 3)],
+                'collection' => 'All',
+                'owner' => 'My studio',
+                'gender' => $gender[rand(0, 5)],
+                'version' => 'basic',
+                'year' => rand(2000, 2022),
+                'fk_id_reviews' => $reviews->id
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
     }
 }
