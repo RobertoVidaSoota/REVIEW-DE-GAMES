3d21b2442d9db381844d759ee3c2390f2554cff6
RobertoVida <roberto50005000@gmail.com>
2022-12-12 12:39:24 -0300

    TEST DEPLOY IN VERCEL -- seg, 12 de dez de 2022 12:39:24

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,9 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'role' => "reviewer",
+            'image_user' => $this->faker->imageUrl(480, 480),
+            'about' => $this->faker->text(50),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,32 +19,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-        $arrayThumb = ArrayBigController::$arrayTumb;
-        $gender = ArrayBigController::$arrayGender;
-        $developer = ['EA', 'Activision', 'Ubisoft', 'Sony'];
+        // $faker = Faker::create();
+        // $arrayThumb = ArrayBigController::$arrayTumb;
+        // $gender = ArrayBigController::$arrayGender;
+        // $developer = ['EA', 'Activision', 'Ubisoft', 'Sony'];
 
-        for($i = 0; $i < 200; $i++)
-        {
-            $reviews = Reviews::create([
-                'name_review' => $faker->text(15),
-                'desc_review' => $faker->text(200),
-                'thumb' => $arrayThumb[rand(0, 31)],
-                'date_review' => date('y-m-d'),
-                'rate' => rand(1, 5),
-                'fk_id_users' => rand(1, 2)
-            ]);
-            $videogame = Videogames::create([
-                'name_game' => $faker->text(10),
-                'developer' => $developer[rand(0, 3)],
-                'collection' => 'All',
-                'owner' => 'My studio',
-                'gender' => $gender[rand(0, 5)],
-                'version' => 'basic',
-                'year' => rand(2000, 2022),
-                'fk_id_reviews' => $reviews->id
-            ]);
-        }
+        // for($i = 0; $i < 200; $i++)
+        // {
+        //     $reviews = Reviews::create([
+        //         'name_review' => $faker->title(),
+        //         'desc_review' => $faker->text(600),
+        //         'thumb' => $arrayThumb[rand(0, 31)],
+        //         'date_review' => date('y-m-d'),
+        //         'rate' => rand(2, 5),
+        //         'fk_id_users' => rand(1, 10)
+        //     ]);
+        //     $videogame = Videogames::create([
+        //         'name_game' => $faker->text(10),
+        //         'developer' => $developer[rand(0, 3)],
+        //         'collection' => 'All',
+        //         'owner' => 'My studio',
+        //         'gender' => $gender[rand(0, 5)],
+        //         'version' => 'basic',
+        //         'year' => rand(2000, 2022),
+        //         'fk_id_reviews' => $reviews->id
+        //     ]);
+        // }
 
         // \App\Models\User::factory(10)->create();
 
