2882f73697425d94178428865535cbf5bb7de8ae
RobertoVida <roberto50005000@gmail.com>
2022-12-12 13:39:57 -0300

    seg, 12 de dez de 2022 13:39:57

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
             input: 'resources/js/app.js',
             refresh: true,
         }),
+        manifestSRI(),
         vue({
             template: {
                 transformAssetUrls: {
